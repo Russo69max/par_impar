be8e5ccd0618efd23ba420e838070e519c3bdfe7
Russo69max <juanpedrofreitas69@gmail.com>
2025-07-02 10:13:23 -0300

    Add files via upload

---
+++ Par_impar.py
@@ -0,0 +1,13 @@
+dc = 1
+while dc != 2:
+    usuario = input("Qual seu nome ")
+    usuario2 = input(f"{usuario} digite um numero inteiro ")
+    par = int("2,4,6 ...")
+    impar = int("1,3,6...")
+    usuario2 = par 
+    print (f"{usuario} seu numero é par ")
+    usuario2 = impar  
+    print (f"{usuario} seu numero é impar ")
+    dc = input(int(f"{usuario} voce quer verificar outro numero? Se sim digite 1- para sim \n 2-para sair"))
+    
+    
